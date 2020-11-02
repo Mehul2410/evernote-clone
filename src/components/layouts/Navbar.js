e3d62b7d8ce19d3ae6227c079cc23b5d0a508aa6
Mehul2410 <mehulgawhale18hc@student.mes.ac.in>
2020-11-02 13:17:07 +0530

    start

---
+++ src/components/layouts/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import MenuIcon from "@material-ui/icons/Menu";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <h1>Evernote</h1>
+      <MenuIcon />
+    </div>
+  );
+};
+
+export default Navbar;
